749e3960b7e6b42047ef1eb28b2a5702c0d6fb1f
Mitja Kurki <mitja.kurki@gmail.com>
2025-09-30 21:37:08 +0000

    - add option to enforce datatypes - fixed longitudinal weight height swap -

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -10,10 +10,10 @@ duckdb -c "COPY (select * from read_csv('finngen_R13_minimum_extended_1.0.txt.gz
 
 duckdb -c "COPY (select * from read_csv('R13_COV_PHENO_V0.txt.gz',delim='\t', nullstr="NA") ) to 'R13_COV_PHENO_V0.parquet' (FORMAT parquet)"
 
+scripts/process_data.R
 duckdb -c "COPY (select * from read_csv('finngen_R13_hilmo_avohilmo_extended_1.0_dedup_fixbp.txt.gz',delim='\t', nullstr="NA") ) to 'finngen_R13_hilmo_avohilmo_extended_1.0_dedup_fixbp.parquet' (FORMAT parquet)"
 
 
 scripts/process_drugs.py finngen_R13_drug_events_2.0.csv finngen_R13_drug_events_2.0.simple.csv
 
-duckdb -c "COPY (select * from read_csv('finngen_R13_drug_events_2.0.simple.csv.gz',delim='\t') order by ATC) to 'finngen_R13_drug_events_2.0.simple.parquet' (FORMAT parquet)"
-
+duckdb -c "COPY (select * from read_csv('finngen_R13_drug_events_2.0.simple.csv.gz',delim='\t' columns = {'VNR'='VARCHAR'}) order by ATC) to 'finngen_R13_drug_events_2.0.simple.parquet' (FORMAT parquet)"
